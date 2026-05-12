b2e8d660aaca6bd3a818b66fb1be518271c012f4
guirrs <guirsan6os189@gmail.com>
2026-05-12 08:26:53 -0300

    feat: organizacao pasta UI

--- UI/royal-games/src/pages/home/index.tsx
+++ UI/royal-games/src/pages/home/index.tsx
@@ -1,11 +1,13 @@
 import Header from "@/components/header/header";
 import styles from "./home.module.css"
+import Footer from "@/components/footer/footer";
 
 const Home = () => {
     return (
         <>
         <div id={styles.container}>
             <Header />
+            <Footer/>
         </div>
         </>
     )
